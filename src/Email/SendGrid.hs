8099320d465df99c51c9f9cc81ce96b6e514520f
William Casarin <bill@casarin.me>
2016-11-04 16:59:34 -0700

    initial commit

---
+++ src/Email/SendGrid.hs
@@ -0,0 +1,9 @@
+
+module Email.SendGrid
+  ( module Email.SendGrid.Client
+  , module Email.SendGrid.Data.Email
+  )
+  where
+
+import Email.SendGrid.Client
+import Email.SendGrid.Data.Email
---
+++ src/Email/SendGrid/Client.hs
@@ -0,0 +1,9 @@
+
+module Email.SendGrid.Client
+    ( sendEmail
+    ) where
+
+import Email.SendGrid.Types
+
+sendEmail :: Email -> IO Response
+sendEmail email = undefined
